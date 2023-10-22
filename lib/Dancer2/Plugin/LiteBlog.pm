318447f554630b2ba84a895dda91af6c48e7c310
Alexis Sukrieh <sukria@sukria.net>
2023-10-22 17:24:48 +0200

    initial import from Module::Starter

---
+++ lib/Dancer2/Plugin/LiteBlog.pm
@@ -0,0 +1,105 @@
+package Dancer2::Plugin::LiteBlog;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Dancer2::Plugin::LiteBlog - The great new Dancer2::Plugin::LiteBlog!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Dancer2::Plugin::LiteBlog;
+
+    my $foo = Dancer2::Plugin::LiteBlog->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Alexis Sukrieh, C<< <sukria at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dancer2-plugin-liteblog at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dancer2-Plugin-LiteBlog>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer2::Plugin::LiteBlog
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer2-Plugin-LiteBlog>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Dancer2-Plugin-LiteBlog>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Dancer2-Plugin-LiteBlog>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2023 by Alexis Sukrieh.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Dancer2::Plugin::LiteBlog
