b3fc07d1d1b016c15ee70b0df09b3b54cdfef680
Alexis Sukrieh <sukria@sukria.net>
2023-10-24 18:53:07 +0200

    Add support for Activities in main class

--- lib/Dancer2/Plugin/LiteBlog.pm
+++ lib/Dancer2/Plugin/LiteBlog.pm
@@ -1,18 +1,7 @@
 package Dancer2::Plugin::LiteBlog;
-use strict;
-use warnings;
-
-use Dancer2::Plugin;
-use Dancer2::Plugin::LiteBlog::Routes;
-use Dancer2::Plugin::LiteBlog::Blog;
-
-use File::Spec;
-use Carp 'croak';
-
 =head1 NAME
 
 Dancer2::Plugin::LiteBlog - A minimalist, file-based blog engine for Dancer2. 
-
 
 =head1 VERSION
 
@@ -22,7 +11,136 @@ Version 0.01
 
 our $VERSION = '0.01';
 
+use strict;
+use warnings;
+use File::Spec;
+use Carp 'croak';
 
+use Dancer2::Plugin;
+use Dancer2::Plugin::LiteBlog::Activities;
+use Dancer2::Plugin::LiteBlog::Routes;
+use Dancer2::Plugin::LiteBlog::Blog;
+
+my $_BLOG;
+sub liteblog_blog {
+    my ($plugin) = @_;
+    return $_BLOG if defined $_BLOG;
+    
+    my $app_config = $plugin->dsl->config->{'liteblog'};
+    my $blogconfig = $app_config->{blog}; 
+    my $blogdir    = $blogconfig->{root}; 
+    if (! defined $blogdir) {
+        $plugin->dsl->info("No blog defined. Set <liteblog.blog.root> in the settings ".
+            "as a valid directory to define it.");
+        return;
+    }
+
+    $_BLOG = Dancer2::Plugin::LiteBlog::Blog->new(rootdir => $blogdir);; 
+    return $_BLOG;
+}
+
+my $_ACTIVITIES;
+sub liteblog_activities {
+    my ($plugin) = @_;
+    return $_ACTIVITIES if defined $_ACTIVITIES;
+
+    my $app_config    = $plugin->dsl->config->{'liteblog'};
+    my $widgetconfig  = $app_config->{activities}; 
+    my $activity_file = $widgetconfig->{content};
+    if (! defined $activity_file) {
+        $plugin->dsl->info("Widget 'Activities' is not enabled. ".
+            "Set <liteblog.activities.content> as a valid YAML file (local to your appdir) ".
+            "to enable it."); 
+        return;
+    }
+
+    $_ACTIVITIES = Dancer2::Plugin::LiteBlog::Activities->new(
+        rootdir => $plugin->dsl->config->{'appdir'},
+        file    => $activity_file,
+    );
+}
+
+=head1 IMPORTED ROUTES
+
+=head2 GET / 
+
+This is a default base route for the site. 
+A Dancer2::Plugin::LiteBlog::Blog singleton is initialized and exposed via the
+liteblog_blog() keyword.
+
+
+=head2 GET /:category/:slug
+
+=cut
+
+sub BUILD {
+    my $plugin = shift;
+
+    $plugin->dsl->info("LiteBlog Init: forcing template_toolkit");
+    $plugin->app->config->{template} = 'template_toolkit';
+
+    # Prepare default template tokens with appropriate resources.
+    $plugin->app->add_hook( Dancer2::Core::Hook->new(
+        name => 'before_template',
+        code => sub {
+            my $tokens = shift;
+            
+            # Each app setting is fowarded to the tokens
+            my $app_config = $plugin->dsl->config->{'liteblog'};
+            $plugin->dsl->info("LiteBlog Init: 'liteblog' config entries are populated ".
+                "to the template tokens", $app_config);
+
+            foreach my $k (keys %$app_config) {
+                $tokens->{$k} = $app_config->{$k};
+            }
+
+            # If the blog engine is enabled, try to fetch featured posts
+            my $posts = [];
+            if (defined $app_config->{blog}) {
+                eval { $posts = liteblog_blog($plugin)->featured_posts };
+                $plugin->dsl->error("Problem with the Blog widget: $@") if $@;
+            }
+            $tokens->{posts} = $posts;
+            $tokens->{has_posts} = scalar(@{$posts});
+
+            # Activity widget
+            my $activities = [];
+            if (defined $app_config->{'activities'}) {
+                eval { 
+                    $activities = liteblog_activities($plugin);
+                    $activities = $activities->content || [];
+                };
+                $plugin->dsl->error("Problem with the Activities widget: $@") if $@;
+            }
+            $tokens->{activities} = $activities;
+            $tokens->{has_activities} = scalar(@{$activities});
+
+            $tokens->{no_widgets} = !($tokens->{has_posts} || $tokens->{has_activities});
+            # set a default title, if unset
+            $tokens->{title} = $app_config->{'title'} || "A Great Liteblog Site" 
+                if !defined $tokens->{title};
+
+            $plugin->dsl->info("LiteBlog: tokens: ", $tokens);
+            return $tokens;
+        }
+    ));
+
+    $plugin->dsl->info("LiteBlog Init: registering route GET /");
+    $plugin->app->add_route(
+        method => 'get',
+        regexp => '/',
+        code   => Dancer2::Plugin::LiteBlog::Routes->index($plugin),
+    );
+    
+    $plugin->dsl->info("LiteBlog Init: registering route GET /blog/:cat/:slug");
+    $plugin->app->add_route(
+        method  => 'get',
+        regexp  => '/blog/:cat/:slug',
+        code    => Dancer2::Plugin::LiteBlog::Routes->post_permalink($plugin),
+    );
+}
+
+1; # End of Dancer2::Plugin::LiteBlog
 =head1 SYNOPSIS
 
 Effortlessly transform your Dancer2 app to db-free blog engine. 
@@ -57,76 +175,6 @@ undef, this means the site will not feature blog posts.
 
 =cut
 
-my $_BLOG;
-sub liteblog_blog {
-    my ($plugin) = @_;
-    return $_BLOG if defined $_BLOG;
-    my $app_config = $plugin->dsl->config->{'app'};
-
-    my $blogdir = $app_config->{blogroot};
-    if (! defined $blogdir) {
-        $plugin->dsl->info("No blog defined. Set <app.blogroot> in the settings as a valid directory to define it.");
-        return;
-    }
-
-    $_BLOG = Dancer2::Plugin::LiteBlog::Blog->new(rootdir => $blogdir);; 
-    return $_BLOG;
-}
-
-=head1 IMPORTED ROUTES
-
-=head2 GET / 
-
-This is a default base route for the site. 
-A Dancer2::Plugin::LiteBlog::Blog singleton is initialized and exposed via the
-liteblog_blog() keyword.
-
-
-=head2 GET /:category/:slug
-
-=cut
-
-sub BUILD {
-    my $plugin = shift;
-
-    # Prepare default template tokens with appropriate resources.
-    $plugin->app->add_hook( Dancer2::Core::Hook->new(
-        name => 'before_template',
-        code => sub {
-            my $tokens = shift;
-            
-            # Each app setting is fowarded to the tokens
-            my $app_config = $plugin->dsl->config->{'app'};
-            foreach my $k (keys %$app_config) {
-                $tokens->{$k} = $app_config->{$k};
-            }
-            $plugin->dsl->info("tokens: ", $tokens);
-
-            # If the blog engine is enabled, try to fetch featured posts
-            my $posts = [];
-            if (defined liteblog_blog($plugin)) {
-                eval { $posts = liteblog_blog($plugin)->featured_posts };
-            }
-            $tokens->{posts} = $posts;
-            $tokens->{has_posts} = scalar(@{$posts});
-
-            # set a default title, if unset
-            $tokens->{title} = "A Great Liteblog Site" if !defined $tokens->{title};
-            return $tokens;
-        }
-    ));
-
-    $plugin->app->add_route(
-        method => 'get',
-        regexp => '/',
-        code   => Dancer2::Plugin::LiteBlog::Routes->index($plugin),
-    );
-    $plugin->app->add_route(
-        method  => 'get',
-        regexp  => '/blog/:cat/:slug',
-        code    => Dancer2::Plugin::LiteBlog::Routes->post_permalink($plugin),
-    );
-}
 
 =head1 AUTHOR
 
@@ -181,4 +229,3 @@ This is free software, licensed under:
 
 =cut
 
-1; # End of Dancer2::Plugin::LiteBlog
