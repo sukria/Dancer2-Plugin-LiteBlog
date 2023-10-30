a0239c3315a2f10c985532a859d8cdcdaa8fb002
Alexis Sukrieh <sukria@sukria.net>
2023-10-30 13:03:06 +0100

    Documentation for Article.pm

--- lib/Dancer2/Plugin/LiteBlog/Article.pm
+++ lib/Dancer2/Plugin/LiteBlog/Article.pm
@@ -1,4 +1,30 @@
 package Dancer2::Plugin::LiteBlog::Article;
+
+=head1 NAME
+
+Dancer2::Plugin::LiteBlog::Article - Represents a single article or page in LiteBlog.
+
+=head1 SYNOPSIS
+
+    use Dancer2::Plugin::LiteBlog::Article;
+
+    my $article = Dancer2::Plugin::LiteBlog::Article->new(
+        basedir => '/path/to/article/directory'
+    );
+
+    print $article->title;
+    print $article->published_date;
+    print $article->content;
+
+=head1 DESCRIPTION
+
+This module provides an object-oriented interface to manage individual articles
+or pages for LiteBlog, a Dancer2-based blogging platform. Each article
+contains metadata and content, parsed from two specific files located in the 
+directory root of the C<Article>: C<meta.yml> and C<content.md>.
+
+=cut
+
 use Moo;
 use Carp 'croak';
 use File::Spec;
@@ -9,6 +35,18 @@ use Text::Markdown 'markdown';
 use File::Slurp;
 use DateTime;
 
+=head1 ATTRIBUTES
+
+=head2 basedir
+
+The base directory where the article files (content.md, meta.yml) reside. Note
+that the directory name is used as the C<slug> of the article. Must be an existing
+directory, read-only and required attribute.
+
+    my $basedir = $article->basedir;
+
+=cut
+
 has basedir => (
     is => 'ro',
     required => 1,
@@ -18,11 +56,26 @@ has basedir => (
     },
 );
 
+=head2 base_path
+
+The base path for articles, when constructing URLs for the articles. 
+Defaults to '/blog'. 
+
+=cut 
+
 has base_path => (
     is => 'ro',
     default => sub { '/blog' },
 );
 
+=head2 slug
+
+Derived from the article directory's name, representing the URL-friendly version
+of the title.
+
+=cut
+
+#TODO: should be cleaned up (no spaces, no non-ASCII chars, etc)
 has slug => (
     is => 'ro',
     lazy => 1,
@@ -31,6 +84,24 @@ has slug => (
         return basename($self->basedir);
     },
 );
+
+=head2 category
+
+Articles directly under the 'articles' directory are considered 'page'.
+Articles that are stored in a sub-directory, below the articles directory 
+are considered articles of that category.
+
+Examples:
+
+    .../articles/some-page # this is a page (top-level article)
+    .../articles/tech/a-blog-post # this is a blog post under the 'tech' category.
+
+Note: this will be changed in future version, the hard-coded value 'articles' 
+will become an attribute of this class so that it can be changed (C<parent_directory>).
+
+=cut
+
+# TODO : 'articles' should become a configurable attribute.
 
 has category => (
     is => 'ro',
@@ -41,6 +112,29 @@ has category => (
         return ($parentdir eq 'articles') ? 'page' : $parentdir;
     },
 );
+
+=head2 is_page
+
+Boolean indicating whether the article is a standalone page, based on the 
+value returned by C<category>.
+
+=cut
+
+has is_page => (
+    is => 'ro',
+    lazy => 1,
+    default => sub {
+        my ($self) = @_;
+        return $self->category eq 'page';
+    },
+);
+
+=head2 published_time
+
+The time the article was published, derived from the content file's modification
+time.
+
+=cut
 
 has published_time => (
     is => 'ro',
@@ -63,6 +157,14 @@ has published_time => (
     }
 );
 
+
+=head2 published_date
+
+Formatted publishing date, e.g., "25 October, 2023", derived from
+C<published_time>.
+
+=cut
+
 has published_date => (
     is => 'ro',
     lazy => 1,
@@ -73,14 +175,14 @@ has published_date => (
         return $dt->strftime('%d %B, %Y');  # e.g., "25 October, 2023"
     },
 );
-has is_page => (
-    is => 'ro',
-    lazy => 1,
-    default => sub {
-        my ($self) = @_;
-        return $self->category eq 'page';
-    },
-);
+
+=head2 meta
+
+A hash reference containing metadata of the article loaded from the 'meta.yml'
+file. That file is supposed to be located within the directory of the Article
+(C<basedir>).
+
+=cut
 
 has meta => (
     is => 'ro',
@@ -95,6 +197,13 @@ has meta => (
     },
 );
 
+=head2 title
+
+The title of the article. Parsed from the content of C<meta.yml>.
+
+=cut
+
+
 has title => (
     is => 'ro',
     lazy => 1,
@@ -103,6 +212,14 @@ has title => (
         return $self->meta->{'title'};
     },
 );
+
+=head2 image
+
+An associated image for the article, if any.
+Parsed from the content of C<meta.yml>.
+
+=cut
+
 
 has image => (
     is => 'ro',
@@ -113,6 +230,13 @@ has image => (
     },
 );
 
+=head2 tags
+
+Array reference of tags associated with the article.
+Parsed from the content of C<meta.yml>.
+
+=cut
+
 has tags => (
     is => 'ro',
     lazy => 1,
@@ -122,6 +246,13 @@ has tags => (
     },
 );
 
+=head2 excerpt
+
+A brief summary or excerpt of the article.
+Parsed from the content of C<meta.yml>.
+
+=cut
+
 has excerpt => (
     is => 'ro',
     lazy => 1,
@@ -130,6 +261,14 @@ has excerpt => (
         return $self->meta->{'excerpt'};
     },
 );
+
+=head2 permalink
+
+The article's unique URL path.
+Derived from C<base_path>, C<category> and C<slug>.
+
+=cut
+
 
 has permalink => (
     is => 'ro',
@@ -142,6 +281,15 @@ has permalink => (
         return join('/', ($self->base_path, $self->category, $self->slug ));
     },
 );
+
+=head2 content
+
+The content of the article, parsed from the associated Markdown file C<content.md>
+and rendered to HTML.
+That file is supposed to be located in the C<basedir> of the Article.
+
+
+=cut
 
 has content => (
     is => 'ro',
@@ -159,3 +307,18 @@ has content => (
 );
 
 1;
+=head1 SEE ALSO
+
+L<Dancer2::Plugin::LiteBlog>, L<Text::Markdown>
+
+=head1 AUTHOR
+
+Alexis Sukrieh, E<lt>sukria@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2023 by Alexis Sukrieh.
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
