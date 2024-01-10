f87e595891ea499256a6e080629742ce6d9ab2bb
Alexis Sukrieh <sukria@gmail.com>
2024-01-10 17:45:14 +0100

    Handle Scrolling events

--- slideshow-poc/script.js
+++ slideshow-poc/script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const slides = document.querySelectorAll(".slideshow-element");
     let currentSlideIndex = 0;
+    let isScrolling = false;
+
+    // should be greater than 400 because the CSS effect is set to 400 ms.
+    let scrollDelay = 410;
+    let shakeDelay = 300;
 
     function setupSlides() {
         slides.forEach((slide, index) => {
@@ -10,9 +15,17 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    function applyShakeEffect() {
+        slides[currentSlideIndex].classList.add('shake');
+        setTimeout(() => {
+            slides[currentSlideIndex].classList.remove('shake');
+        }, shakeDelay);
+    }
+
     function changeSlide(nextSlideIndex) {
-        if (nextSlideIndex >= 0 && nextSlideIndex < slides.length) {
-            
+        if (!isScrolling && nextSlideIndex >= 0 && nextSlideIndex < slides.length) {
+            isScrolling = true;
+
             // Next slide takes the lead
             slides[nextSlideIndex].classList.add('active');
 
@@ -41,17 +54,36 @@ document.addEventListener("DOMContentLoaded", function() {
                     slides[currentSlideIndex].classList.add('above');
                 }
 
+                setTimeout(() => {
+                    isScrolling = false;
+                }, 800); 
+
                 currentSlideIndex = nextSlideIndex;
-            }, 500); // Durée de la transition
+            }, scrollDelay); 
+        }
+
+        // at the first or the last slide
+        else if (!isScrolling) {
+            applyShakeEffect();
         }
     }
 
     setupSlides();
 
+    // Handle key strokes
     document.addEventListener("keydown", function(event) {
         if (event.key === 'ArrowDown') {
             changeSlide(currentSlideIndex + 1);
         } else if (event.key === 'ArrowUp') {
+            changeSlide(currentSlideIndex - 1);
+        }
+    });
+
+    // Handle wheel scroll
+    document.addEventListener("wheel", function(event) {
+        if (event.deltaY > 0) {
+            changeSlide(currentSlideIndex + 1);
+        } else if (event.deltaY < 0) {
             changeSlide(currentSlideIndex - 1);
         }
     });
